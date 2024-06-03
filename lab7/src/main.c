6ffc91e422c17c99ad6500d47867df808f50f82f
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-06-03 17:14:56 +0300

    Add files via upload

--- lab7/src/main.c
+++ lab7/src/main.c
@@ -1,166 +1,174 @@
-#include <stdio.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <unistd.h>
 
-struct record_s{
+#define MAX_RECORDS 10
+
+typedef struct record_s {
     char name[80];
     char address[80];
-    u_int8_t semester;
-};
-int get_index(int fd,struct record_s* original){
-    struct record_s record;
-    int index = 0;
-    while (read(fd, &record, sizeof(record)) == sizeof(record)) {
-        if(strcmp(record.name,original->name) == 0 && strcmp(record.address,original->address) == 0 && record.semester == original->semester){
-            return index;
-        }
-        index++;
-    }
-    return index;
+    int semester;
+} record_t;
+
+int fd;
+struct flock lock;
+
+void print_record(int rec_no) {
+    record_t record;
+    off_t offset = rec_no * sizeof(record);
+    lseek(fd, offset, SEEK_SET);
+    read(fd, &record, sizeof(record));
+    printf("%d: %s, %s, %d\n", rec_no, record.name, record.address, record.semester);
 }
-int is_record_locked(int fd, int rec_no) {
-    struct flock lock;
+
+void get_record(int rec_no, record_t *record) {
+    off_t offset = rec_no * sizeof(*record);
+    lseek(fd, offset, SEEK_SET);
+    read(fd, record, sizeof(*record));
+}
+
+void modify_record(int rec_no, record_t *record) {
+    off_t offset = rec_no * sizeof(*record);
+    lseek(fd, offset, SEEK_SET);
+    write(fd, record, sizeof(*record));
+}
+
+void save_record(record_t *record, record_t *new_record, int rec_no) {
+    if (rec_no < 0 || rec_no >= MAX_RECORDS) {
+        printf("Invalid record number\n");
+        return;
+    }
+
     lock.l_type = F_WRLCK;
     lock.l_whence = SEEK_SET;
-    lock.l_start = rec_no * sizeof(struct record_s);
-    lock.l_len = sizeof(struct record_s);
-
-    if (fcntl(fd, F_GETLK, &lock) == -1) {
-        // Ошибка при вызове fcntl()
-        return 1;
-    }
-
-    return lock.l_type != F_UNLCK;
-}
-void lock_record(int fd, int rec_no, int lock_type) {
-    struct flock lock;
-    lock.l_type = lock_type;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = rec_no * sizeof(struct record_s);
-    lock.l_len = sizeof(struct record_s);
-    while (fcntl(fd, F_SETLKW, &lock) == -1 && errno == EINTR);
-}
-
-void unlock_record(int fd, int rec_no) {
-    struct flock lock;
-    lock.l_type = F_UNLCK;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = rec_no * sizeof(struct record_s);
-    lock.l_len = sizeof(struct record_s);
-    fcntl(fd, F_SETLK, &lock);
-}
-void put_record(int fd, int rec_no, struct record_s *record) {
-    lseek(fd, rec_no * sizeof(struct record_s), SEEK_SET);
-    write(fd, record, sizeof(struct record_s));
-}
-struct record_s* get_record(int fd, int rec_no) {
-    if(is_record_locked(fd,rec_no)){
-        printf("Запись залочена\n");
-        return NULL;
-    }
-    struct record_s *record = malloc(sizeof (struct record_s));
-    lseek(fd, rec_no * sizeof(struct record_s), SEEK_SET);
-    read(fd, record, sizeof(struct record_s));
-    return record;
-}
-int compare_record(struct record_s* record,struct record_s* original){
-    if(strcmp(record->name,original->name) == 0 && strcmp(record->address,original->address) == 0 && record->semester == original->semester)
-    {
-        return 1;
-    }
-    return 0;
-}
-
-void display_records(int fd) {
-    for(int i = 0;i<10;i++){
-        if(is_record_locked(fd,i)){
-            printf("В файле есть лок\n");
-            return;
+    lock.l_start = rec_no * sizeof(*record);
+    lock.l_len = sizeof(*record);
+    while (fcntl(fd, F_SETLK, &lock) == -1) {
+        if (errno == EAGAIN) {
+            printf("Record %d is locked. Waiting...\n", rec_no);
+            sleep(1);
+        } else {
+            perror("fcntl");
+            exit(1);
         }
     }
-    struct record_s record;
-    int index = 0;
-    while (read(fd, &record, sizeof(record)) == sizeof(record)) {
-        printf("Record %d:\n", index++);
-        printf("  Name: %s\n", record.name);
-        printf("  Address: %s\n", record.address);
-        printf("  Semester: %d\n", record.semester);
+    sleep(2);
+
+    record_t current_record;
+    lseek(fd, rec_no * sizeof(current_record), SEEK_SET);
+    if (read(fd, &current_record, sizeof(current_record)) == -1) {
+        perror("read");
+        return;
     }
 
-    close(fd);
+    if (strcmp(current_record.name, record->name) != 0 || strcmp(current_record.address, record->address) != 0 || current_record.semester != record->semester) {
+        printf("Record %d has been modified by another process. Trying again...\n", rec_no);
+        get_record(rec_no, record);
+        lock.l_type = F_UNLCK;
+        fcntl(fd, F_SETLK, &lock);
+        save_record(record, new_record, rec_no);
+    } else {
+        printf("Writing...\n");
+        modify_record(rec_no, new_record);
+        lock.l_type = F_UNLCK;
+        fcntl(fd, F_SETLK, &lock);
+    }
 }
-int main() {
-    char file_name[80];
-    printf("Введите имя файла\n");
-    scanf("%s",file_name);
-    while (1){
-        struct record_s* original;
-        struct record_s* record;
-        int fd = open(file_name,O_RDWR);
-        getchar();
-        char select = 0;
-        printf("1. LST\n2. get\n3. mod\n4. quit\n");
-        scanf("%c",&select);
-        if(select == '1'){
-            display_records(fd);
-        }
-        if(select == '2'){
-            int n = 0;
-            printf("Введите индекс\n");
-            scanf("%d",&n);
-            if(n<0 || n>9){
-                printf("Выход за пределы массива\n");
-                return 0;
+
+void fileCreate(const char *filename) {
+    struct record_s arrayRecords[10];
+
+    for (int i = 0; i < 10; i++) {
+        sprintf(arrayRecords[i].name, "Name_%c", 'A' + i);
+        sprintf(arrayRecords[i].address, "Address%d", i);
+        arrayRecords[i].semester = i % 2 + 1;
+    }
+    FILE *file;
+    if ((file = fopen(filename, "r")) != NULL) {
+        fclose(file);
+        return;
+    }
+
+    file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("file");
+        return;
+    }
+    fwrite(arrayRecords, sizeof(arrayRecords), 1, file);
+    fclose(file);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: ./main <filename>");
+        return -1;
+    }
+
+    fileCreate(argv[1]);
+
+    fd = open(argv[1], O_RDWR);
+    if (fd == -1) {
+        perror("Filename");
+        exit(1);
+    }
+
+    printf("File opened successfully\n");
+
+    printf("Menu:\nLST - list all records\nGET - get a record\nPUT - save the last modified record\nEXIT - exit the program\n");
+
+    char command[4];
+    int rec_no = -1;
+    record_t current_record;
+
+    while (1) {
+        printf(">");
+        scanf("%s", command);
+        if (strcasecmp(command, "LST") == 0) {
+            printf("Records:\n");
+            for (int i = 0; i < MAX_RECORDS; i++) {
+                print_record(i);
             }
-            record = get_record(fd,n);
-            if(record == NULL){
+        } else if (strcmp(command, "GET") == 0) {
+            printf("Record number: ");
+            scanf("%d", &rec_no);
+
+            get_record(rec_no, &current_record);
+
+            printf("Record %d: %s, %s, %d\n", rec_no, current_record.name, current_record.address, current_record.semester);
+        } else if (strcasecmp(command, "PUT") == 0) {
+            if (rec_no == -1) {
+                printf("No records was getted\n");
                 continue;
             }
-            printf("Record %d:\n", n);
-            printf("  Name: %s\n", record->name);
-            printf("  Address: %s\n", record->address);
-            printf("  Semester: %d\n", record->semester);
-            close(fd);
 
-        }
-        if(select == '3'){
-            int n = 0;
-            printf("Введите индекс\n");
-            scanf("%d",&n);
-            if(is_record_locked(fd,n)){
-                printf("Запись залочена\n");
-                continue;
-            }
-            lock_record(fd,n,F_RDLCK);
-            if(n<0 || n>9){
-                printf("Выход за пределы массива\n");
-                return 0;
-            }
-            record = get_record(fd,n);
-            char c;
-            scanf("%s",&c);
-            printf("Old semester = %d\n",record->semester);
-            record->semester++;
-            printf("New semester = %d\n",record->semester);
-            put_record(fd,n,record);
-            unlock_record(fd,n);
-            close(fd);
-        }
-        if(select == '4'){
-            close(fd);
+            record_t new_record;
+
+            get_record(rec_no, &current_record);
+
+            printf("Name: ");
+            scanf("%s", new_record.name);
+
+            printf("Address: ");
+            scanf("%s", new_record.address);
+
+            printf("Semester: ");
+            scanf("%d", &new_record.semester);
+
+            save_record(&current_record, &new_record, rec_no);
+
+            printf("Record %d saved successfully\n", rec_no);
+
+        } else if (strcasecmp(command, "EXIT") == 0) {
+            printf("Exiting program...\n");
             break;
+        } else {
+            printf("Invalid command\n");
         }
-
     }
-
-
-
-
+    close(fd);
     return 0;
 }
